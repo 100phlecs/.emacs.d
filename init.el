c9ce713928c7e71ebc68a1468682f153942503e5
100phlecs <hello@based.town>
2021-10-22 23:48:21 -0400

    set up dart lsp

--- init.el
+++ init.el
@@ -2,10 +2,12 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
+(toggle-frame-maximized)
 (set-fringe-mode 10)
 (setq ns-auto-hide-menu-bar nil)
-(set-face-attribute 'default nil :family "Iosevka" :height 170)
-
+(set-face-attribute 'default nil :family "Iosevka Term" :height 170)
+(set-face-attribute 'fixed-pitch nil :family "Iosevka Fixed" :height 170)
+(set-face-attribute 'variable-pitch nil :family "Iosevka" :height 170)
 
 
 (defvar bootstrap-version)
@@ -44,7 +46,7 @@
   :bind
   ([remap describe-function] . counsel-describe-function)
   ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-discribe-variable)
+  ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
 ;; Lookup the current symbol at point. C-c C-d is a common keybinding
@@ -58,7 +60,7 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-solarized-dark t)
+  (load-theme 'doom-solarized-light t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -131,3 +133,81 @@
 ;; need to set up
 ;; to browse issues & PRs
 (use-package forge)
+
+
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (auto-fill-mode 0)
+  (visual-line-mode 1))
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ⤵"
+	org-hide-emphasis-markers t))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode))
+
+
+(use-package lsp-mode
+  :commands (lsp lsp-deffered)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :config
+  (lsp-enable-which-key-integration t))
+(use-package lsp-dart)
+(use-package dart-mode)
+(use-package dap-mode)
+(use-package flycheck)
+(use-package company)
+(use-package lsp-treemacs)
+(use-package lsp-ui)
+
+(setq lsp-dart-sdk-dir "/Users/100phlecs/packages/flutter/bin/cache/dart-sdk")
+(setq lsp-dart-flutter-sdk-dir "/Users/100phlecs/packages/flutter")
+(add-hook 'dart-mode-hook 'lsp)
+(setq lsp-dart-dap-flutter-hot-reload-on-save t)
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+that used by the user's shell.
+
+This is particularly useful under Mac OS X and macOS, where GUI
+apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$" "" (shell-command-to-string
+					  "$SHELL --login -c 'echo $PATH'"
+						    ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(defcustom asdf-path "$HOME/.asdf"
+  "Path to asdf directory."
+  :type 'string
+  :group 'asdf)
+
+(defcustom asdf-binary "$HOME/.asdf/bin/asdf"
+  "Path to asdf binary."
+  :type 'string
+  :group 'asdf)
+
+(defun asdf-enable ()
+  "Setup asdf for environment."
+  (interactive)
+  (let ((path (substitute-env-vars (concat asdf-path "/shims:" asdf-path "/bin:$PATH"))))
+    (setenv "PATH" path)
+    (setq exec-path
+          (append
+           (split-string-and-unquote path ":")
+           exec-path))))
+
+(asdf-enable)
+
+(set-exec-path-from-shell-PATH)
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      company-minimum-prefix-length 1
+      lsp-lens-enable t
+      lsp-signature-auto-activate nil)
