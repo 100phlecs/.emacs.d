4b21c8ce675ca30b305ad85f316d9c05a38440de
100phlecs <hello@based.town>
2021-11-13 23:43:22 -0500

    fix org load order, enable lispy

--- init.el
+++ init.el
@@ -392,7 +392,7 @@ folder, otherwise delete a word."
     ;;;; 4. locate-dominating-file
   ;; (setq consult-project-root-function (lambda () (locate-dominating-file "." ".git")))
   )
-
+(require 'consult)
 (use-package consult-yasnippet
   :bind ("C-x C-y" . consult-yasnippet))
 
@@ -556,11 +556,15 @@ folder, otherwise delete a word."
   (advice-add 'org-refile :after 'org-save-all-org-buffers)
   :bind ("C-c a" . org-agenda))
 
+(use-package org-download)
+(add-hook 'dired-mode-hook 'org-download-enable)
+
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode))
 
 (use-package org-roam
+  :after consult
   :straight t
   :demand
   :init
@@ -569,15 +573,11 @@ folder, otherwise delete a word."
   (org-roam-directory "~/Documents/notes")
   (org-roam-completion-everywhere t)
 
-  (defun phl-org-roam-rg ()
-    "Search across the content of the root org dir."
-    (interactive)
-    (consult-ripgrep org-roam-directory))
-
   (org-roam-dailies-capture-templates
    '(("d" "default" entry "* %<%I:%M %p>: %?"
       :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
 
+  (phl-template-init)
 
   (org-roam-capture-templates
    `(("d" "default" plain
@@ -606,6 +606,12 @@ folder, otherwise delete a word."
   :bind-keymap
   ("C-c n d" . org-roam-dailies-map)
   :config
+
+  (defun phl-org-roam-rg ()
+    "Search across the content of the root org dir."
+    (interactive)
+    (consult-ripgrep org-roam-directory))
+
   (require 'org-roam-dailies) ;; Ensure the keymap is available
   (org-roam-db-autosync-mode)
   (org-roam-setup))
@@ -643,100 +649,102 @@ folder, otherwise delete a word."
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'phl-org-babel-tangle-config)))
 
-(defun phl-org-roam-filter-by-tag (tag-name)
-  (lambda (node)
-    (member tag-name (org-roam-node-tags node))))
+(defun phl-template-init ()
 
-(defun phl-org-roam-list-notes-by-tag (tag-name)
-  (mapcar #'org-roam-node-file
-          (seq-filter
-           (phl-org-roam-filter-by-tag tag-name)
-           (org-roam-node-list))))
+  (defun phl-org-roam-filter-by-tag (tag-name)
+        (lambda (node)
+          (member tag-name (org-roam-node-tags node))))
 
-(defun phl-org-roam-refresh-agenda-list ()
-  (interactive)
-  (setq org-agenda-files (append (phl-org-roam-list-notes-by-tag "Project")
-                                 '("~/Documents/notes/agenda/Tasks.org"
-                                   "~/Documents/notes/agenda/Habits.org"))
+      (defun phl-org-roam-list-notes-by-tag (tag-name)
+        (mapcar #'org-roam-node-file
+                (seq-filter
+                 (phl-org-roam-filter-by-tag tag-name)
+                 (org-roam-node-list))))
+
+      (defun phl-org-roam-refresh-agenda-list ()
+        (interactive)
+        (setq org-agenda-files (append (phl-org-roam-list-notes-by-tag "Project")
+                                       '("~/Documents/notes/agenda/Tasks.org"
+                                         "~/Documents/notes/agenda/Habits.org"))
+              )
         )
-  )
 
-;; Build the agenda list the first time for the session
-(phl-org-roam-refresh-agenda-list)
+      ;; Build the agenda list the first time for the session
+      (phl-org-roam-refresh-agenda-list)
 
-(defun phl-org-roam-project-finalize-hook ()
-  "Adds the captured project file to `org-agenda-files' if the
-         capture was not aborted."
-  ;; Remove the hook since it was added temporarily
-  (remove-hook 'org-capture-after-finalize-hook #'phl-org-roam-project-finalize-hook)
+      (defun phl-org-roam-project-finalize-hook ()
+        "Adds the captured project file to `org-agenda-files' if the
+               capture was not aborted."
+        ;; Remove the hook since it was added temporarily
+        (remove-hook 'org-capture-after-finalize-hook #'phl-org-roam-project-finalize-hook)
 
-  ;; Add project file to the agenda list if the capture was confirmed
-  (unless org-note-abort
-    (with-current-buffer (org-capture-get :buffer)
-      (add-to-list 'org-agenda-files (buffer-file-name)))))
+        ;; Add project file to the agenda list if the capture was confirmed
+        (unless org-note-abort
+          (with-current-buffer (org-capture-get :buffer)
+            (add-to-list 'org-agenda-files (buffer-file-name)))))
 
-(defun phl-org-roam-find-project ()
-  (interactive)
-  ;; Add the project file to the agenda after capture is finished
-  (add-hook 'org-capture-after-finalize-hook #'phl-org-roam-project-finalize-hook)
+      (defun phl-org-roam-find-project ()
+        (interactive)
+        ;; Add the project file to the agenda after capture is finished
+        (add-hook 'org-capture-after-finalize-hook #'phl-org-roam-project-finalize-hook)
 
-  ;; Select a project file to open, creating it if necessary
-  (org-roam-node-find
-   nil
-   nil
-   (phl-org-roam-filter-by-tag "Project")
-   :templates
-   '(("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
-      :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project")
-      :unnarrowed t))))
+        ;; Select a project file to open, creating it if necessary
+        (org-roam-node-find
+         nil
+         nil
+         (phl-org-roam-filter-by-tag "Project")
+         :templates
+         '(("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+            :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+category: ${title}\n#+filetags: Project")
+            :unnarrowed t))))
 
-(defun phl-org-roam-capture-inbox ()
-  (interactive)
-  (org-roam-capture- :node (org-roam-node-create)
-                     :templates '(("i" "inbox" plain "* %?"
-                                   :if-new (file+head "Inbox.org" "#+title: Inbox\n")))))
+      (defun phl-org-roam-capture-inbox ()
+        (interactive)
+        (org-roam-capture- :node (org-roam-node-create)
+                           :templates '(("i" "inbox" plain "* %?"
+                                         :if-new (file+head "Inbox.org" "#+title: Inbox\n")))))
 
-(defun phl-org-roam-capture-project-task ()
-  (interactive)
-  ;; Add the project file to the agenda after capture is finished
-  (add-hook 'org-capture-after-finalize-hook #'phl-org-roam-project-finalize-hook)
+      (defun phl-org-roam-capture-project-task ()
+        (interactive)
+        ;; Add the project file to the agenda after capture is finished
+        (add-hook 'org-capture-after-finalize-hook #'phl-org-roam-project-finalize-hook)
 
-  ;; Capture the new task, creating the project file if necessary
-  (org-roam-capture- :node (org-roam-node-read
-                            nil
-                            (phl-org-roam-filter-by-tag "Project"))
-                     :templates '(("p" "project" plain "** TODO %?"
-                                   :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org"
-                                                          "#+title: ${title}\n#+category: ${title}\n#+filetags: Project"
-                                                          ("Tasks"))))))
+        ;; Capture the new task, creating the project file if necessary
+        (org-roam-capture- :node (org-roam-node-read
+                                  nil
+                                  (phl-org-roam-filter-by-tag "Project"))
+                           :templates '(("p" "project" plain "** TODO %?"
+                                         :if-new (file+head+olp "%<%Y%m%d%H%M%S>-${slug}.org"
+                                                                "#+title: ${title}\n#+category: ${title}\n#+filetags: Project"
+                                                                ("Tasks"))))))
 
-(defun phl-org-roam-copy-todo-to-today ()
-  (interactive)
-  (let ((org-refile-keep t) ;; Set this to nil to delete the original!
-        (org-roam-dailies-capture-templates
-         '(("t" "tasks" entry "%?"
-            :if-new (file+head+olp "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n" ("Tasks")))))
-        (org-after-refile-insert-hook #'save-buffer)
-        today-file
-        pos)
-    (save-window-excursion
-      (org-roam-dailies--capture (current-time) t)
-      (setq today-file (buffer-file-name))
-      (setq pos (point)))
+      (defun phl-org-roam-copy-todo-to-today ()
+        (interactive)
+        (let ((org-refile-keep t) ;; Set this to nil to delete the original!
+              (org-roam-dailies-capture-templates
+               '(("t" "tasks" entry "%?"
+                  :if-new (file+head+olp "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n" ("Tasks")))))
+              (org-after-refile-insert-hook #'save-buffer)
+              today-file
+              pos)
+          (save-window-excursion
+            (org-roam-dailies--capture (current-time) t)
+            (setq today-file (buffer-file-name))
+            (setq pos (point)))
 
-    ;; Only refile if the target file is different than the current file
-    (unless (equal (file-truename today-file)
-                   (file-truename (buffer-file-name)))
-      (org-refile nil nil (list "Tasks" today-file nil pos)))))
+          ;; Only refile if the target file is different than the current file
+          (unless (equal (file-truename today-file)
+                         (file-truename (buffer-file-name)))
+            (org-refile nil nil (list "Tasks" today-file nil pos)))))
 
-(add-to-list 'org-after-todo-state-change-hook
-             (lambda ()
-               (when (equal org-state "DONE")
-                 (phl-org-roam-copy-todo-to-today))))
+      (add-to-list 'org-after-todo-state-change-hook
+                   (lambda ()
+                     (when (equal org-state "DONE")
+                       (phl-org-roam-copy-todo-to-today))))
 
-(global-set-key (kbd "C-c n t") #'phl-org-roam-capture-project-task)
-(global-set-key (kbd "C-c n n") #'phl-org-roam-capture-inbox)
-(global-set-key (kbd "C-c n p") #'phl-org-roam-find-project)
+      (global-set-key (kbd "C-c n t") #'phl-org-roam-capture-project-task)
+      (global-set-key (kbd "C-c n n") #'phl-org-roam-capture-inbox)
+      (global-set-key (kbd "C-c n p") #'phl-org-roam-find-project))
 
 (defun phl-start-new-eshell ()
   "Spawn a new eshell always."
@@ -801,6 +809,8 @@ folder, otherwise delete a word."
 
 (use-package lispy)
 (setq lispy-use-sly t)
+(add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1)))
+(add-hook 'lisp-mode-hook (lambda () (lispy-mode 1)))
 
 (use-package rustic
   :bind (:map rustic-mode-map
